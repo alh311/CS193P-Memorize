9aa5fc18db573d32769c899b524b0cbb4fd46a60
Alex H <alh311@gmail.com>
2022-10-06 15:19:01 -0600

    App creation

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alex Hutchins on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
