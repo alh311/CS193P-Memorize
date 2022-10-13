afcb21b1e3d5cebfe75702abbc07dfccf13f75c1
Alex H <alh311@gmail.com>
2022-10-13 13:38:44 -0600

    Lecture 4 * Adds game logic * Wires up UI to VM

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,50 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = ["✈️", "🏎", "🚀", "🚜", "🚗", "🚌", "🚒", "🚓", "🚛", "🛵", "🏍", "🛺", "🛸", "🚁", "🛶", "⛵️", "🚤", "🚋", "🚠", "🚕"]
-    @State var emojiCount = 16
+    @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        VStack {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                        CardView(content: emoji)
-                            .aspectRatio(2/3, contentMode: .fit)
-                    }
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                ForEach(viewModel.cards) { card in
+                    CardView(card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
                 }
             }
-            .foregroundColor(.red)
         }
+        .foregroundColor(.red)
         .padding(.horizontal)
     }
 }
 
 
 struct CardView: View {
-    var content: String
-    @State var isFaceUp: Bool = true
+    let card: MemoryGame<String>.Card
     
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
             
-            if isFaceUp {
+            if card.isFaceUp {
                 shape
                     .fill()
                     .foregroundColor(.white)
                 shape
                     .strokeBorder(lineWidth: 3)
-                Text(content)
+                Text(card.content)
                     .font(.largeTitle)
+            }
+            else if card.isMatched {
+                shape.opacity(0)
             }
             else {
                 shape
                     .fill()
             }
-        }
-        .onTapGesture {
-            isFaceUp = !isFaceUp
         }
     }
 }
@@ -59,10 +58,12 @@ struct CardView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let game = EmojiMemoryGame()
+        
+        ContentView(viewModel: game)
             .previewDisplayName("Dark")
             .preferredColorScheme(.dark)
-        ContentView()
+        ContentView(viewModel: game)
             .previewDisplayName("Light")
             .preferredColorScheme(.light)
     }
--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 
 
-class EmojiMemoryGame {
+class EmojiMemoryGame: ObservableObject {
     
     static let emojis = ["✈️", "🏎", "🚀", "🚜", "🚗", "🚌", "🚒", "🚓", "🚛", "🛵", "🏍", "🛺", "🛸", "🚁", "🛶", "⛵️", "🚤", "🚋", "🚠", "🚕"]
     
@@ -19,9 +19,15 @@ class EmojiMemoryGame {
         }
     }
     
-    private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model: MemoryGame<String> = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
     }
 }
